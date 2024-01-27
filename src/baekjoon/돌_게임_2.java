2bfe7fe80adc563cfd5ac69f5e0f9b9b45ff736f
최수원 <suwon3274@gmail.com>
2024-01-28 03:54:54 +0900

    Solved 1 problem on baekjoon

---
+++ src/baekjoon/돌_게임_2.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌_게임_2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(n%2==0 ? "SK" : "CY");
+    }
+
+}
